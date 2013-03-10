d2af3b389a98f0e2ea49cca37236ab6e9d029d1b
Mingmin Xie <melvinxie@gmail.com>
2013-03-10 22:24:39 +0900

    init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+    @currencies = ['usd', 'aud', 'cad', 'chf', 'cny', 'dkk', 'eur', 'gbp',
+                   'hkd', 'jpy', 'mxn', 'nzd', 'php', 'sek', 'sgd', 'thb',
+                   'zar']
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heroku::Application.config.secret_key_base = 'c7237ade53f27d87a10b6eacb0d447f39cca840a202d645d5fa627ddef617d6fc2cf6c9328e49120f12ed4046be69e1dca2706e376fd18901f198eee0ee43b8a'
