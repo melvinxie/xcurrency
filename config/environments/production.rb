c61f7da4430603f6aa2b18ba866864423c1ef5c6
Mingmin Xie <melvinxie@gmail.com>
2013-03-20 23:43:30 +0900

    allow iframe embedding

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,11 @@ Currency::Application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
+  config.action_dispatch.default_headers = {
+    'X-XSS-Protection' => '1; mode=block',
+    'X-Content-Type-Options' => 'nosniff'
+  }
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
