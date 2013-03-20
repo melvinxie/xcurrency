19b7273a494d3fdc7e3cb9b86e58e31b12e78305
Mingmin Xie <melvinxie@gmail.com>
2013-03-21 01:05:41 +0900

    don't reset X-Frame-Options...

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -1,6 +1,5 @@
 class CurrencyController < ApplicationController
   def index
-    response.headers['X-Frame-Options'] = 'GOFORIT'
     @currencies = ['usd', 'aud', 'cad', 'chf', 'cny', 'dkk', 'eur', 'gbp', 'hkd', 'jpy', 'mxn',
                    'nzd', 'php', 'sek', 'sgd', 'thb', 'zar']
   end
