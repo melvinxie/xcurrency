6b872a2d1176789fce19c38a2f837a56167f7795
Mingmin Xie <melvinxie@gmail.com>
2013-03-21 00:07:36 +0900

    delete X-Frame-Options

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -1,6 +1,6 @@
 class CurrencyController < ApplicationController
   def index
-    response.headers['X-Frame-Options'] = ''
+    response.headers.delete('X-Frame-Options')
     @currencies = ['usd', 'aud', 'cad', 'chf', 'cny', 'dkk', 'eur', 'gbp', 'hkd', 'jpy', 'mxn',
                    'nzd', 'php', 'sek', 'sgd', 'thb', 'zar']
   end
