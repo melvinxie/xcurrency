891f891e47b0e2077c21e4768a6eb2f959dcd223
Mingmin Xie <melvinxie@gmail.com>
2013-03-23 22:57:50 +0900

    text

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -2,7 +2,7 @@ class CurrencyController < ApplicationController
   def index
     @currencies = ActiveSupport::OrderedHash.new
     @currencies['Americas'] = ['usd', 'cad', 'mxn', 'brl', 'cop']
-    @currencies['Europe/Middle East/Africa'] =
+    @currencies['Europe / Middle East / Africa'] =
         ['eur', 'gbp', 'chf', 'nok', 'sek', 'dkk', 'rub', 'zar', 'ils', 'aed']
     @currencies['Asia'] =
         ['jpy', 'cny', 'hkd', 'inr', 'myr', 'sgd', 'krw', 'twd', 'aud', 'nzd']
